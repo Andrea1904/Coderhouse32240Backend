11f9b17224c437292f6dc7da8358b97b5f92c5ab
Andrea1904 <alopezm@bancodeoccidente.com.co>
2023-03-09 20:40:04 -0500

    Ruteo Avanzado

---
+++ Clase 23 - Ruteo Avanzado/src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import UsersRouter from './routes/users.router.js';
+import SesionRouter from './routes/session.router.js'
+
+const app = express();
+app.use(express.json());
+app.listen(8080,()=>console.log("Server arriba"));
+
+const usersRouter = new UsersRouter();
+app.use('/users',usersRouter.getRouter());
+
+const sesionRouter = new SesionRouter();
+app.use('/sesion',sesionRouter.getRouter());
+
